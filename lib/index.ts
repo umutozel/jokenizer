49d196aad195bced7dc081756acf0c50350c1e08
umutozel <umutozel@gmail.com>
2018-07-22 19:36:39 +0300

    create tokenize function

--- lib/index.ts
+++ lib/index.ts
@@ -1,9 +1,26 @@
 export default function tokenize(exp: string) {
+    let idx = 0;
+
     return null;
 }
 
-export interface IToken {
+export const enum Operator {
+
 }
 
-export enum TokenType {
+const operators = {
+
+}
+
+export interface IExpression {
+    type: ExpressionType;
+}
+
+export interface IBinaryExpression extends IExpression {
+    operator: Operator;
+    left: IExpression;
+    right: IExpression;
+}
+
+export const enum ExpressionType {
 }
