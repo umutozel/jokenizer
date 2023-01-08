5652ee77e8722ed8244881ec06059af547650531
Umut Özel <umutozel@gmail.com>
2023-01-08 18:35:59 +0300

    fix encoding

--- lib/evaluator.ts
+++ lib/evaluator.ts
@@ -3,8 +3,8 @@ import { Settings } from "./Settings";
 import { Expression } from "./shared";
 import { tokenize } from "./Tokenizer";
 
-export function evaluate<T = any>(exp: Expression | string, ...scopes: any[]): T;
-export function evaluate<T = any>(exp: Expression | string, settings: Settings, ...scopes: any[]): T {
+export function evaluate<T = any>(exp: Expression | string, ...scopes: any[]): T;
+export function evaluate<T = any>(exp: Expression | string, settings: Settings, ...scopes: any[]): T {
     if (!(settings instanceof Settings)) {
         scopes = [settings, ...scopes];
         settings = void 0;
